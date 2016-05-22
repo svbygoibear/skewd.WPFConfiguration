c931fd1e8e6501654b8e454d41792a3d73e3b599
simoneslatnpou <svbyogibear@gmail.com>
2016-05-22 22:45:07 +0200

    Added a PropertyWrapper class in the Wrappers namespace.

--- skewd.WPFConfiguration/Wrappers/PropertyWrapper.cs
+++ skewd.WPFConfiguration/Wrappers/PropertyWrapper.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace skewd.WPFConfiguration.Wrappers
-{
-    class PropertyWrapper
-    {
+namespace skewd.WPFConfiguration.Wrappers {
+    [Serializable]
+    public class PropertyWrapper {
+        public object WrappedObject { get; set; }
+
+        public PropertyWrapper() { }
+
+        public PropertyWrapper(object wrappedObject) : this() {
+            WrappedObject = (WrappedObject != wrappedObject) ? wrappedObject : WrappedObject;
+        }
     }
 }
